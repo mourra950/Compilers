1b3173e0a601e06a2f4ed0dd6a0da5b8a5779c85
Ahmed Sameh <ahmedelsherefy@gmail.com>
2023-05-15 17:40:43 +0300

    Updated Grammar.txt

--- src/statementsBody.py
+++ src/statementsBody.py
@@ -1,1 +1,22 @@
 from utils import *
+
+def WriteBody(indexPointer):
+    Children = []
+    output = dict()
+
+    out1 = WriteArgument(indexPointer)
+    Children.append(out1["node"])
+
+    out2 = writeBodyDash(out1["index"])
+    Children.append(out2["node"])
+
+    out3 = Match(Token_type.End, out2["index"])
+    Children.append(out3["node"])
+
+    out4 = Match(Token_type.Semicolon, out3["index"])
+    Children.append(out4["node"])
+
+    Node = Tree("Header", Children)
+    output["node"] = Node
+    output["index"] = out4["index"]
+    return output
