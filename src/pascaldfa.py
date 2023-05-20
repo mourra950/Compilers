36d03e9ff67b6f123467fb959fdda7d35d871421
mourra950 <mourra24@gmail.com>
2023-05-20 23:23:10 +0300

    starting dfa
    Co-Authored-By: AdhamHaythem <104657468+AdhamHaythem@users.noreply.github.com>
    Co-Authored-By: Ahmed Sameh <89102517+AhmedSameh52@users.noreply.github.com>
    Co-Authored-By: Obsessi0n9 <105242821+Obsessi0n9@users.noreply.github.com>
    Co-Authored-By: Mohamed Amr <105242221+mohameedamrr@users.noreply.github.com>

--- src/pascaldfa.py
+++ src/pascaldfa.py
@@ -1,14 +1,40 @@
 from automata.fa.dfa import DFA
-
-
+"""
+'And', 'Array', 'Begin', 'Case', 'Const',
+'Div', 'DownTo', 'Else', 'End', 'File', 'For',
+'Function', 'GoTo', 'If', 'In', 'Label', 'Mode',
+'Nil', 'Not', 'Of', 'Or', 'Packed', 'Procedure', 'Program',
+'Record', 'Repeat', 'Set', 'Then', 'To', 'Type', 'Until', 'Var',
+'While', 'With', 'Do', 'PlusOp', 'MinusOp', 'MultiplyOp', 'DivideOp',
+'LessEqual', 'GreatEqual', 'Equal', 'Greater', 'Lesser',
+'OpenMultiComment', 'CloseMultiComment', 'OpenComment', 'CloseComment',
+'OpenGroup', 'CloseGroup', 'Semicolon', 'Colon', 'Assignment', 'Str', 'Identifier',
+'Uses', 'Comma', 'Constant', 'Real', 'Char', 'Boolean', 'Error',
+'Read', 'ReadLn', 'Write', 'WriteLn', 'Integer', 'Int', 'RealType',
+'CharType', 'StringType', 'BooleanType'
+"""
+"""
+1- program 
+2- program name
+3- semicolomn
+"""
 dfa = DFA(
-    states={'program', 'header', 'declaration',
-            'execution','librarySection' ,'4', '5', '6', 'dead'},
-    input_symbols={'+', '-', '/', '*', 'ID', 'NUM'},
+    states={'start', '1', '2', '3' 'end', 'error'},
+    input_symbols={'Program', 'Identifier', 'Semicolon', 'AND', 'ARRAY'},
     transitions={
-        'q0': {'0': 'q0', '1': 'q1'},
-        'q1': {'0': 'q0', '1': 'q2'},
-        'q2': {'0': 'q2', '1': 'q1'}},
-    initial_state='q0',
-    final_states={'q1'}
+        'start': {'Program': '1', 'Identifier': 'error', 'Semicolon': 'error', 'AND': 'error', 'ARRAY': 'error'},
+        '1': {'Program': 'error', 'Identifier': '2', 'Semicolon': 'error', 'AND': 'error', 'ARRAY': 'error'},
+        '2': {'Program': 'error', 'Identifier': 'error', 'Semicolon': '3', 'AND': 'error', 'ARRAY': 'error'},
+        '3': {'Program': 'error', 'Identifier': 'error', 'Semicolon': 'error', 'AND': 'error', 'ARRAY': 'error'},
+
+        'end': {'Program': 'error', 'Identifier': 'error', 'Semicolon': 'error', 'AND': '1', 'ARRAY': 'error'},
+        'error': {'Program': 'error', 'Identifier': 'error', 'Semicolon': 'error', 'AND': 'error', 'ARRAY': 'error'}
+
+    },
+    initial_state='start',
+    final_states={'1', 'error'}
 )
+token = ['PROGRAM', 'AND']
+steps = dfa.read_input_stepwise(token)
+for i in steps:
+    print(i)
